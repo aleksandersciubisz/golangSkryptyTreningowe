241b91e5a02fedd9bffcf2e705cc33ff13d4b4b4
aleksandersciubisz <aleksander.sciubisz@gmail.com>
2017-05-27 13:16:28 +0200

    update1

--- math.go
+++ math.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"math/big"
 )
 
 func main() {
-	
+
 	i1, i2, i3 := 12, 45, 68
 	intSum := i1 + i2 + i3
 	fmt.Println("Integer sum: ", intSum)
-	
+
+	f1, f2, f3 := 12.3, 45.4, 68.77
+	int2Sum := f1 + f2 + f3
+	fmt.Println("Floating sum: ", int2Sum)
+
+	var b1, b2, b3, bigSum big.Float
+	b1.SetFloat64(23.5)
+	b2.SetFloat64(3.6)
+	b3.SetFloat64(77.11)
+
+	bigSum.Add(&b1, &b2).Add(&bigSum, &b3)
+	fmt.Printf("big sum: %10g\n", &bigSum)
+
 }
--- strings.go
+++ strings.go
@@ -2,9 +2,25 @@ package main
 
 import (
 	"fmt"
-//	"strings"
+	//"strconv"
+	"strings"
 )
 
 func main() {
-	
+	str1 := "Implicit"
+	var str2 string = "Explicit"
+	fmt.Println(str1)
+	fmt.Println(str2)
+	fmt.Printf("Implicit: %v\n Explicit: %v\n",
+		str1, str2)
+	fmt.Printf("typ str1 %T\n", str1)
+
+	fmt.Println("pokaz string package")
+	fmt.Println(strings.ToUpper(str2))
+
+	lvalue := "Hello"
+	lValue := "hello"
+	fmt.Println("Equal?", (lValue == lvalue))
+	fmt.Println("Equal? Non-case sensitive",
+		strings.EqualFold(lValue, lvalue))
 }
